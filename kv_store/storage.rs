249e3664f3a0025e88ebf209269ab8433accf0bf
pen0x1 <163335431+pen0x1@users.noreply.github.com>
2024-07-29 19:17:20 +0200

    change names

--- kv_store/storage.rs
+++ kv_store/storage.rs
@@ -7,70 +7,71 @@ use std::io::{self, BufReader, BufWriter, Read, Write};
 use std::path::PathBuf;
 use std::str::FromStr;
 
-const STORAGE_FILE_ENV: &str = "KV_STORE_PATH";
+const STORAGE_FILE_PATH_ENV_VAR: &str = "KV_STORE_PATH";
 
 #[derive(Serialize, Deserialize)]
-struct KvStore {
-    store: HashMap<String, String>,
-    storage_path: PathBuf,
+struct KeyValueStore {
+    data: HashMap<String, String>,
+    file_path: PathBuf,
 }
 
-impl KvStore {
+impl KeyValueStore {
     fn new() -> Result<Self, Box<dyn std::error::Error>> {
-        let storage_path = env::var(STORAGE_FILE_ENV).map_err(|_| "KV_STORE_PATH must be set and valid")?;
-        let path = PathBuf::from_str(&storage_path)?;
-        let store = if path.exists() {
-            KvStore::load_from_file(&path)?
+        let storage_path_str = env::var(STORAGE_FILE_PATH_ENV_VAR)
+            .map_err(|_| "Environment variable KV_STORE_PATH must be set and valid")?;
+        let file_path = PathBuf::from_str(&storage_path_str)?;
+        let data = if file_path.exists() {
+            KeyValueStore::load_from_file(&file_path)?
         } else {
             HashMap::new()
         };
-        Ok(KvStore {
-            store,
-            storage_path: path,
+        Ok(KeyValueStore {
+            data,
+            file_path,
         })
     }
 
-    fn load_from_file(path: &PathBuf) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
-        let file = File::open(path)?;
+    fn load_from_file(file_path: &PathBuf) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
+        let file = File::open(file_path)?;
         let reader = BufReader::new(file);
-        let store = serde_json::from_reader(reader)?;
-        Ok(store)
+        let data = serde_json::from_reader(reader)?;
+        Ok(data)
     }
 
     fn save_to_file(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let file = File::create(&self.storage_path)?;
+        let file = File::create(&self.file_path)?;
         let writer = BufWriter::new(file);
-        serde_json::to_writer(writer, &self.store)?;
+        serde_json::to_writer(writer, &self.data)?;
         Ok(())
     }
 
     fn add(&mut self, key: String, value: String) -> Result<(), Box<dyn std::error::Error>> {
-        self.store.insert(key, value);
+        self.data.insert(key, value);
         self.save_to_file()
     }
 
-    fn get(&self, key: &str) -> Option<&String> {
-        self.store.get(key)
+    fn retrieve(&self, key: &str) -> Option<&String> {
+        self.data.get(key)
     }
 
-    fn delete(&mut self, key: String) -> Result<(), Box<dyn std::error::Error>> {
-        self.store.remove(&key);
+    fn remove(&mut self, key: String) -> Result<(), Box<dyn std::error::Error>> {
+        self.data.remove(&key);
         self.save_to_file()
     }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut kv_store = KvStore::new()?;
+    let mut kv_store = KeyValueStore::new()?;
     kv_store.add("Key1".to_string(), "Value1".to_string())?;
     kv_store.add("Key2".to_string(), "Value2".to_string())?;
 
-    if let Some(value) = kv_store.get("Key1") {
+    if let Some(value) = kv_store.retrieve("Key1") {
         println!("Value for 'Key1': {}", value);
     }
 
-    kv_store.delete("Key1".to_string())?;
-    if kv_store.get("Key1").is_none() {
-        println!("Key1 is successfully deleted");
+    kv_store.remove("Key1".to_string())?;
+    if kv_store.retrieve("Key1").is_none() {
+        println!("Key1 is successfully removed");
     }
 
     Ok(())
